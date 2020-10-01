0194f5112381c9b69804bdabe57ad9fa94ac88b6
Nick Heiner <nheiner@netflix.com>
2020-10-01 13:36:45 -0700

    Update snapshots with codemod that takes CLI args

--- fixtures/prepend-string/codemod/codemod.js
+++ fixtures/prepend-string/codemod/codemod.js
@@ -3,7 +3,8 @@ module.exports = {
   postProcess: modifiedFiles => {
     console.log('codemod post process', JSON.stringify(modifiedFiles));
   },
-  transform({source}) {
+  transform({source, commandLineArgs}) {
+    console.log('commandLineArgs', JSON.stringify(commandLineArgs));
     return `/* prefix prepend string */\n${source}`;
   }
 };
