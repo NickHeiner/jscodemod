e753d28ef37eef9034c35fdef13126ffa38b5f84
Nick Heiner <nheiner@netflix.com>
2021-02-22 16:51:36 -0500

    Hacking

--- fixtures/arrow-function-inline-return/codemod/index.ts
+++ fixtures/arrow-function-inline-return/codemod/index.ts
@@ -1,14 +1,22 @@
-import {Codemod} from '@nth/jscodemod';
+// import {Codemod} from '@nth/jscodemod';
 import babelPlugin from './babel-plugin';
-import {transformSync} from '@babel/core';
+import babel from '@babel/core';
+import {parse, print} from 'recast';
 
-const codemod: Codemod = {
-  transform({source, filePath}) {
-    return transformSync(source, {
+const codemod: any = {
+  transform({source, filePath}: {source: string, filePath: string}) {
+    const ast = parse(source, {parser: babel});
+    const result = babel.transformFromAstSync(ast, source, {
       filename: filePath,
       plugins: ['@babel/plugin-syntax-optional-chaining', '@babel/plugin-syntax-typescript', babelPlugin],
       ast: true
-    })?.code;
+    });
+    if (!result) {
+      throw new Error(`Transforming "${filePath}" resulted in a null babel result.`);
+    }
+
+    // @ts-ignore
+    return print(result.ast).code;
   }
 };
 
