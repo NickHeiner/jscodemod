5322092b3a9f443e52fce4404d9852fc35ce36e7
Nick Heiner <nheiner@netflix.com>
2021-02-23 12:14:31 -0500

    Add note about how to run TS fixture locally

--- fixtures/arrow-function-inline-return/codemod/index.ts
+++ fixtures/arrow-function-inline-return/codemod/index.ts
@@ -1,4 +1,7 @@
+// To run this codemod from the jscodemod repo while developing locally, change this import to:
+// import {Codemod} from '../../..';
 import {Codemod} from '@nth/jscodemod';
+
 import babelPlugin from './babel-plugin';
 import {transformSync} from '@babel/core';
 
