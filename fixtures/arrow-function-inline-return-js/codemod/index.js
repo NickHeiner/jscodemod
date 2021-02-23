0b2bb04254a1e59ec5a28ad966efdfdb86971b7b
Nick Heiner <nheiner@netflix.com>
2021-02-23 12:10:38 -0500

    Hacking but it does not work

--- fixtures/arrow-function-inline-return-js/codemod/index.js
+++ fixtures/arrow-function-inline-return-js/codemod/index.js
@@ -1,19 +1,39 @@
 const babelPlugin = require('./babel-plugin');
 const babel = require('@babel/core');
 const {parse, print} = require('recast');
+const _ = require('lodash');
 
 module.exports = {
   transform({source, filePath}) {
-    const ast = parse(source, {parser: babel});
-    const result = babel.transformFromAstSync(ast, source, {
+    return babel.transformSync(source, {
       filename: filePath,
       plugins: ['@babel/plugin-syntax-optional-chaining', '@babel/plugin-syntax-typescript', babelPlugin],
       ast: true
-    });
-    if (!result) {
-      throw new Error(`Transforming "${filePath}" resulted in a null babel result.`);
-    }
+    }).code;
+    // const babelOpts = {
+    //   filename: filePath,
+    //   plugins: ['@babel/plugin-syntax-optional-chaining', '@babel/plugin-syntax-typescript', babelPlugin],
+    //   ast: true
+    // };
+    
+    // const parser = {
+    //   parse(source, opts) {
+    //     return babel.parse(source, {
+    //       ...babelOpts,
+    //       ..._.omit(
+    //         opts, 
+    //         'jsx', 'loc', 'locations', 'range', 'comment', 'onComment', 'tolerant', 'ecmaVersion'
+    //       )
+    //     });
+    //   }
+    // };
+    
+    // const ast = parse(source, {parser});
+    // const result = babel.transformFromAstSync(ast, source, babelOpts);
+    // if (!result) {
+    //   throw new Error(`Transforming "${filePath}" resulted in a null babel result.`);
+    // }
 
-    return print(result.ast).code;
+    // return print(result.ast).code;
   }
 };
