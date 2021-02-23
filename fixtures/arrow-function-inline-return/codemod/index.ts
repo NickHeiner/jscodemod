933c6d384b608858a2c928974fd9610ed24b2c62
Nick Heiner <nheiner@netflix.com>
2021-02-23 15:14:32 -0500

    Fix test and update snapshot

--- fixtures/arrow-function-inline-return/codemod/index.ts
+++ fixtures/arrow-function-inline-return/codemod/index.ts
@@ -1,24 +1,22 @@
-/* @ts-nocheck */
-
 import {Codemod} from '@nth/jscodemod';
-import babelPlugin from './babel-plugin';
-// import {parse as babelParse} from '@babel/core';
-import {parse as babelParse, transformFromAstSync} from '@babel/core';
+import babelPlugin, {TODO} from './babel-plugin';
+import {parse as babelParse, TransformOptions, transformSync} from '@babel/core';
 import {parse, print} from 'recast';
 import _ from 'lodash';
+import type {Visitor} from '@babel/traverse';
 
 const codemod: Codemod = {
   transform({source, filePath}: {source: string, filePath: string}) {
-    const babelOpts = {
+    const getBabelOpts = (plugins: Exclude<TransformOptions['plugins'], null> = []): TransformOptions => ({
       filename: filePath,
-      plugins: ['@babel/plugin-syntax-optional-chaining', '@babel/plugin-syntax-typescript', babelPlugin],
+      plugins: [...plugins, '@babel/plugin-syntax-optional-chaining', '@babel/plugin-syntax-typescript', babelPlugin],
       ast: true
-    };
+    });
     
     const parser = {
       parse(source: string, opts: Record<string, unknown>) {
         return babelParse(source, {
-          ...babelOpts,
+          ...getBabelOpts(),
           ..._.omit(
             opts, 
             'jsx', 'loc', 'locations', 'range', 'comment', 'onComment', 'tolerant', 'ecmaVersion'
@@ -28,20 +26,22 @@ const codemod: Codemod = {
     };
     
     const ast = parse(source, {parser});
-    const result = transformFromAstSync(ast, source, babelOpts);
+
+    const setAst = (): {visitor: Visitor<TODO>} => ({
+      visitor: {
+        Program(path) {
+          path.replaceWith(ast.program);
+        }
+      }
+    });
+
+    const result = transformSync('', getBabelOpts([setAst]));
     if (!result) {
       throw new Error(`Transforming "${filePath}" resulted in a null babel result.`);
     }
 
-    const fileName = require('path').basename(filePath);
-
-    require('fs').writeFileSync(`/Users/nheiner/code/jscodemod/recast-${fileName}.json`, JSON.stringify(ast, null, 2));
-    require('fs').writeFileSync(`/Users/nheiner/code/jscodemod/transformed-${fileName}.json`, JSON.stringify(result.ast, null, 2));
-
-    // // @ts-ignore
-    // return print(result.ast).code;
-
-    return print(ast).code;
+    // @ts-ignore
+    return print(result.ast).code;
   }
 };
 
