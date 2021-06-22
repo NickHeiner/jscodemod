d3465e30ad73d493498f2443b764f31205cb15c7
Nick Heiner <nheiner@netflix.com>
2021-06-22 12:02:01 -0400

    Pull in getPlugin functionality from detect branch

--- src/worker.ts
+++ src/worker.ts
@@ -2,9 +2,16 @@ import getLogger from './get-logger';
 import piscina from 'piscina';
 import fs from 'fs';
 import loadCodemod from './load-codemod';
-import type {CodemodResult} from './types';
+import type {CodemodResult, TODO} from './types';
 import _ from 'lodash';
 import getCodemodName from './get-codemod-name';
+import {
+  parse as babelParse, 
+  transformSync as babelTransformSync, 
+  Visitor, 
+  TransformOptions
+} from '@babel/core';
+import * as recast from 'recast';
 
 const baseLog = getLogger({
   name: 'jscodemod-worker',
@@ -35,15 +42,91 @@ export default async function main(sourceCodeFile: string): Promise<CodemodMetaR
 
   let transformedCode: CodemodResult;
   let threwError = false;
-  try {
-    transformedCode = await codemod.transform!({
-      source: originalFileContents, 
-      filePath: sourceCodeFile, 
-      commandLineArgs: parsedArgs
-    });
-  } catch (e) {
+
+  const handleError = (e: Error, messageSuffix: string) => {
     threwError = true;
-    log.error({error: _.pick(e, 'message', 'stack')}, `Codemod "${codemodName}" threw an error for a file.`);
+    log.error({error: _.pick(e, 'message', 'stack')}, `Codemod "${codemodName}" threw an error ${messageSuffix}`);
+  };
+
+  const codemodOpts = {
+    source: originalFileContents, 
+    filePath: sourceCodeFile, 
+    commandLineArgs: parsedArgs,
+  };
+
+  if (codemod.transform) {
+    try {
+      transformedCode = await codemod.transform(codemodOpts);
+    } catch (e) {
+      handleError(e, 'for a file');
+    }
+  } else {
+    // TODO: This can probably be cleaned up.
+    // TODO: This makes some excessive changes:
+    //    * Paren insertion: https://github.com/benjamn/recast/issues/914
+    //    * Reformatting `return\n(expr)` to `return expr`. This doesn't repro with recast-only.
+    //    * If a file is entirely commented out, it'll be deleted.
+    //    * A trailing comment will have a space removed:
+    //          `a; /*f*/` => `a;/*f*/`
+    // 
+    // The impact of this would be reduced if we detected when the AST is unchanged, and then did not write new
+    // file contents. However, this proved difficult to do.
+    // 
+    // eslint-disable-next-line max-len
+    // Maybe we want the parserOverrides approach: https://github.com/codemod-js/codemod/blob/06310982b67783e9d2861a7737c7810396417bd3/packages/core/src/RecastPlugin.ts.
+
+    let codemodPlugins; 
+    try {
+      codemodPlugins = await codemod.getPlugin(codemodOpts);
+    } catch (e) {
+      handleError(e, 'when callling codemod.getPlugin()');
+    }
+    
+    const pluginsToUse = Array.isArray(codemodPlugins) ? codemodPlugins : [codemodPlugins]; 
+
+    const getBabelOpts = (extraPlugins: Exclude<TransformOptions['plugins'], null> = []): TransformOptions => ({
+      filename: sourceCodeFile,
+      plugins: [...extraPlugins, ...pluginsToUse],
+      ast: true
+    });
+
+    const parser = {
+      parse(source: string, opts: Record<string, unknown>) {
+        return babelParse(source, {
+          ...getBabelOpts(),
+          ..._.pick(codemod, 'presets'),
+          // There are options that are recognized by recast but not babel. Babel errors when they're passed. To avoid
+          // this, we'll omit them.
+          ..._.omit(
+            opts, 
+            'jsx', 'loc', 'locations', 'range', 'comment', 'onComment', 'tolerant', 'ecmaVersion'
+          )
+        });
+      }
+    };
+
+    const ast = recast.parse(originalFileContents, {parser});
+
+    const setAst = (): {visitor: Visitor<TODO>} => ({
+      visitor: {
+        Program(path) {
+          path.replaceWith(ast.program);
+        }
+      }
+    });
+
+    // result.ast.end will be 0, and ast.end is originalFileContents.length.
+    // Passing originalFileContents instead of '' solves that problem, but causes some other problem.
+    const result = babelTransformSync('', getBabelOpts([setAst]));
+    if (!result) {
+      throw new Error(`Transforming "${sourceCodeFile}" resulted in a null babel result.`);
+    }
+
+    transformedCode = recast.print(result.ast as recast.types.ASTNode).code;
+
+    if (originalFileContents.endsWith('\n') && !transformedCode.endsWith('\n')) {
+      transformedCode += '\n';
+    }    
   }
 
   const codeModified = Boolean(transformedCode && transformedCode !== originalFileContents);
