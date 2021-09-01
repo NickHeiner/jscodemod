38ba587ac58895b539d5ce416add2df7ef951e4a
Nick Heiner <nheiner@netflix.com>
2021-09-01 16:21:20 -0400

    Use log instead of baseLog

--- src/run-codemod-on-file.ts
+++ src/run-codemod-on-file.ts
@@ -42,16 +42,18 @@ export default async function runCodemodOnFile(
     timeSinceRunStartPretty: prettyMs(timeSinceRunStart)
   });
 
-  const originalFileContents = await baseLog.logPhase(
+  // TODO The nth-log types are messed up.
+  // @ts-expect-error
+  const originalFileContents = await log.logPhase(
     {phase: 'read file', level: 'trace'},
     () => pFs.readFile(sourceCodeFile, 'utf-8')
   );
   const rawArgs = codemodArgs ? JSON.parse(codemodArgs) : undefined;
-  const parsedArgs = await baseLog.logPhase({
+  // TODO The nth-log types are messed up.
+  // @ts-expect-error
+  const parsedArgs = await log.logPhase({
     phase: 'parse args',
     level: 'trace'
-  // TODO The types are messed up.
-  // @ts-expect-error
   }, () => codemod.parseArgs?.(rawArgs));
 
   const codemodOpts = {
@@ -237,7 +239,9 @@ export default async function runCodemodOnFile(
   let thrownError = null;
 
   try {
-    codemodResult = await baseLog.logPhase({phase: 'transform file', level: 'trace'}, transformFile);
+    // TODO The nth-log types are messed up.
+    // @ts-expect-error
+    codemodResult = await log.logPhase({phase: 'transform file', level: 'trace'}, transformFile);
   } catch (e) {
     thrownError = e;
     log.error({
