9be9069d91ce629e4a5ea9a526cf042d365eedf3
Nick Heiner <nheiner@netflix.com>
2021-09-27 18:14:52 -0400

    Be less weird about how I am setting the timeout

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -23,7 +23,7 @@ const log = createLog({name: 'test'});
 // Tests run slower on GH CI.
 // Disable this lint rule because it's obvious what the number refers to.
 // eslint-disable-next-line no-magic-numbers
-jest.setTimeout(60 * 2000);
+jest.setTimeout(120 * 1000);
 
 type TestArgs = {
   fixtureName: string;
