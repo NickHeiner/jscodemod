239e62c1d7292dcef5d7459de5e0b1ce25bf0f15
Nick Heiner <nheiner@netflix.com>
2022-12-22 13:39:38 -0800

    Fix Node 14 compat issue

--- src/build-full-prompt.ts
+++ src/build-full-prompt.ts
@@ -1,5 +1,10 @@
+// We need to use a polyfill because replaceAll only started being available in Node 15.
+// I'm not sure what the right way to import this is according to TS, but this works.
+// @ts-expect-error
+import replaceAll from 'core-js/es/string/replace-all';
+
 export default function buildFullPrompt(prompt: string, sourceCode: string): string {
-  const promptWithInputSubstitued = prompt.replaceAll('{{INPUT_SOURCE_CODE}}', sourceCode);
+  const promptWithInputSubstitued = replaceAll(prompt, '{{INPUT_SOURCE_CODE}}', sourceCode);
   const wrappedPrompt = prompt.includes('\n')
     ? `/*\n${promptWithInputSubstitued}\n*/\n` : `${sourceCode}\n\n/*${prompt}*/`;
   return `${wrappedPrompt}\n`;
