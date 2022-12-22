7a86014b4e1a30fd13112994c995ac708c315a68
Nick Heiner <nheiner@netflix.com>
2022-12-22 13:43:33 -0800

    Update fixtures

--- fixtures/ai/codemod/5to6.ts
+++ fixtures/ai/codemod/5to6.ts
@@ -7,7 +7,7 @@ const codemod = {
       prompt: `
         ${source}
 
-        // The Javascript code above is written in ES5. Here's what it looks like translated to modern JS:
+        /* The Javascript code above is written in ES5. Transform it to ES6. Include only the transformed code; do not include any other comments, context, or metadata. Do not create new comments. Make sure to apply the following transformations: 1. Use optional chaining. 2. Use the object property shorthand. */
       `
     }
   },
