bc5f0dac04b95d29876ce76a04bd274a5ff419b8
Nick Heiner <nheiner@netflix.com>
2023-01-12 14:15:48 -0500

    Expand the test

--- __tests__/run-ai-codemod.ts
+++ __tests__/run-ai-codemod.ts
@@ -14,11 +14,15 @@ describe('OpenAIAPIRateLimiter', () => {
     const getNextRequest = () => ({makeRequest, estimatedTokens});
     const tokenRateLimit = 20;
     const rateLimiter = new OpenAIAPIRateLimiter(log, 3, tokenRateLimit, getNextRequest);
+
+    // Making a single call should result in a request.
     rateLimiter.attemptCall();
     jest.advanceTimersToNextTimer();
     expect(makeRequest).toHaveBeenCalledTimes(1);
     makeRequest.mockClear();
 
+    // We need jest.advanceTimersToNextTimer() because of the debounce on attemptCall, which I'm not sure is actually 
+    // necessary.
     rateLimiter.attemptCall();
     jest.advanceTimersToNextTimer();
 
@@ -34,11 +38,13 @@ describe('OpenAIAPIRateLimiter', () => {
     rateLimiter.attemptCall();
     jest.advanceTimersToNextTimer();
 
+    // We only expect 3 calls, because the calls above will exceed the local rate limit.
     expect(makeRequest).toHaveBeenCalledTimes(3);
     makeRequest.mockClear();
 
     jest.advanceTimersToNextTimer(Infinity);
 
+    // Once the rate limit clears, we expect the rate limiter to retry automatically.
     expect(makeRequest).toHaveBeenCalledTimes(1);
     makeRequest.mockClear();
 
@@ -51,6 +57,26 @@ describe('OpenAIAPIRateLimiter', () => {
     rateLimiter.attemptCall();
     jest.advanceTimersToNextTimer();
 
+    // We make two calls, but only expect one to go through, because the first call used too many tokens.
     expect(makeRequest).toHaveBeenCalledTimes(1);
+    makeRequest.mockClear();
+
+    jest.advanceTimersByTime(60_000);
+
+    makeRequest.mockImplementation(() => Promise.resolve({tokensUsed: 5, rateLimitReached: true}));
+    rateLimiter.attemptCall();
+    jest.advanceTimersToNextTimer();
+    rateLimiter.attemptCall();
+    jest.advanceTimersToNextTimer();
+
+    // We expect the first call to have occurred. It results in a rateLimitReached response, so the rate limiter 
+    // enqueues a retry.
+    expect(makeRequest).toHaveBeenCalledTimes(1);
+
+    jest.advanceTimersToNextTimer(Infinity);
+
+    // Validate that the retry occurred.
+    expect(makeRequest).toHaveBeenCalledTimes(2);
+    makeRequest.mockClear();
   });
 });
